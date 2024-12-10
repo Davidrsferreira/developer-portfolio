07de101e40e8c523f5fe3debeb6773a0911b7604
David Ferreira <davidrsferreira@gmail.com>
2024-12-10 09:42:24 -0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,8 +4,8 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is DAVID. I am a professional and enthusiastic Software Developer with 10+ years of experience. I am a quick learner with a self-learning attitude. I love working with web application development and my core skill is based on Java and Typescript. I am available for a job opportunity that suits my skills and interests.",
   email: 'davidrsferreira@gmail.com',
-  phone: '+351 933 422 777',
-  address: 'Porto, Portugal ',
+  phone: '+55 (16) 99711-6610',
+  address: 'Brazil',
   github: 'https://github.com/davidrsferreira',
   linkedIn: 'https://www.linkedin.com/in/davidrsferreira/',
   resume: "https://drive.google.com/file/d/1plRS1QZr14WCvSOw1KvFwVjMtMEx66lU/view?usp=drive_link"
